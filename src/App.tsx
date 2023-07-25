158ec4d28a95072f313278f75f29624097ed3bab
divyuk <div14kes@gmail.com>
2023-07-25 10:40:01 +0530

    Basic Structure of Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
-  return <div>Hi</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<h1>Home</h1>} />
+      <Route path="/new" element={<h1>New</h1>} />
+      <Route path="/:id">
+        <Route index element={<h1>Show</h1>} />
+        <Route path="edit" element={<h1>Edit</h1>} />
+      </Route>
+      <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
+  );
 }
 
 export default App;
